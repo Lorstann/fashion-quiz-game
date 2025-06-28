04c60f5d50b6a51915bc73f5e8c610b17284cfdf
U210709008 <mustafakurtar61@gmail.com>
2025-06-29 01:08:42 +0300

    Updates

--- src/components/ScoreCounter.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function ScoreCounter({ score, anim }) {
-  return (
-    <div className={`score-counter${anim ? ` ${anim}` : ""}`}>Score: {score}</div>
-  );
-} 
